24d7cd2d1403245257d6181b62f36596d57053e7
andrewoc98 <60736542+andrewoc98@users.noreply.github.com>
2021-03-04 15:59:21 +0000

    Update train.py

--- FYP/PredictionModel/train.py
+++ FYP/PredictionModel/train.py
@@ -4,70 +4,102 @@ import numpy as np
 import sys
 import datetime as dt
 import tensorflow as tf
-import model
-import tflearn.model_selection as tf
-def trainModel(model,inputs,labels):
+def trainModel(model,input1,input2,labels):
+    input1=np.asarray(input1).astype('float32')
+    input2=np.asarray(input2).astype('float32')
+    labels=np.asarray(labels).astype('float32')
     
-    model.fit(x=inputs,y=labels,epochs=20, validation_split=0.2)
-    model.save(filepath='../PredictionModel/PredictionModel.bin')
+    model.fit({'I1':input1,'I2':input2},labels,epochs=20, validation_split=0.2,verbose =1)
+    model.save(filepath='../FYP/PredictionModel/PredictionModel.bin')
     print("I work")
     
 def getData():
-
+    today=str(dt.datetime.now().strftime("%x"))
+    today = today.replace('/','-')
     #labels
-    PriceDf= pd.read_csv('../TweetModel/TweetFolder/Price.csv')
-    Pricedf = Pricedf.tolist()
+    PriceDf= pd.read_csv('../FYP/TweetModel/TweetFolder/Price.csv')
+    PriceDf = PriceDf.values.tolist()
     SentimentList=[]
     NewsList=[]
 
     #Sentiments
-    for filename in os.listdir('../TweetModel/TweetFolder/'):
+    for filename in os.listdir('../FYP/TweetModel/TweetFolder/'):
         if filename.endswith('Tweet.csv'):
-            df = pd.read_csv(f'../TweetModel/TweetFolder/{filename}',header=None)
-            df,splits= normaliseData(df)
-            SentimentList.append([df,filename[:(len(filename)-9)]])
+            if(filename !=f'{today}Tweet.csv'):
+                df = pd.read_csv(f'../FYP/TweetModel/TweetFolder/{filename}',header=None)
+                df= normaliseData(str(filename[:(len(filename)-9)]),df)
+                SentimentList.append(df)
+                
+
     
     #news
-    for filename in os.listdir('../TweetModel/TweetFolder/'):
+    for filename in os.listdir('../FYP/TweetModel/TweetFolder/'):
         if filename.endswith('News.csv'):
-            df=pd.read_csv(f'../TweetModel/TweetFolder/{filename}',header=None)
-            NewsList.append([df,filename[:(len(filename)-9)]])
+            if(filename !=f'{dt.date.today().isoformat()}News.csv'):
+                df=pd.read_csv(f'../FYP/TweetModel/TweetFolder/{filename}',header=None)
+                NewsList.append([filename[:(len(filename)-9)],df])
 
 
-    inputs,labels =ShapeData(SentimentList,NewsList,PriceDf,splits)
-    return inputs,labels
+    
+    input1,input2,labels =ShapeData(SentimentList,NewsList,PriceDf)
+    return input1,input2,labels
     
 
 
-def normaliseData(dataframe):
+def normaliseData(Date,dataframe):
+    output=[]
     dataframe=dataframe.values.tolist()
     splits=int(len(dataframe)/10)
     drops=(len(dataframe)%10)
     del dataframe[(len(dataframe)-drops):len(dataframe)]
-    return np.split(dataframe,splits),splits
+    dataframe=np.asarray(dataframe)
+    dataframe = np.split(dataframe,splits)
+    for Sentiment in dataframe:
+        output.append([Sentiment,Date])
+    return output
 
 
 
-def ShapeData(SentimentList,NewsList,PriceList,numofarray):
-    inputs=[]
+
+
+def ShapeData(SentimentList,NewsList,PriceList):
+    input1=[]
+    input2=[]
     labels=[]
+    
     #Allign Sentiments and news
-    for Sentiment in SentimentList:
-        for News in NewsList:
-            if Sentiment[1]==NewsList[1] :
-                NewsArr=[NewsList[0]]*numofarray
-                inputs.append([Sentiment[0],NewsArr])
-                break
-        
-        for Label in PriceList:
-            if Sentiment[1]==PriceList[1]:
-                labels.append([Label]*numofarray)
-                break
-            
+    for News in NewsList:
+        News[1]=News[1].values.tolist()
+        for File in SentimentList:
+            for Sentiment in File:
+                NewsDate=DateConvert(News[0])
+                if str(Sentiment[1])==NewsDate :
+                    try:
 
-    return inputs,labels
+                        
+                        News[1]=np.asarray(News[1])
+                        News[1]=np.squeeze(News[1])
+                        Sentiment[0]=np.squeeze(Sentiment[0])
+                        News[1]=News[1].tolist()
+                        Sentiment[0]=Sentiment[0].tolist()
+                        input1.append(Sentiment[0])
+                        input2.append(News[1])
+                        for Label in PriceList:
+                            if News[0]==Label[1]:
+                                labels.append(Label[2]) 
+                                
+                    except:
+                        print(News[1])
+                        print(Sentiment[0])
+                        print(Label[2])
+                        break
+
+    return input1,input2,labels
 
 
-
-def predictStock(inputs,model):
-    return model.predict(inputs)
+def DateConvert(Date):
+    Date=str(Date)
+    Date=Date[2:]
+    Date=Date[:-3]+'-'+Date[-2:]
+    Date=Date[3:]+'-'+Date[:2]
+    return Date
