8f130e17257c60eda02709dc1729d357a7f3cba6
andrewoc98 <60736542+andrewoc98@users.noreply.github.com>
2021-04-07 16:02:05 +0100

    Update config.py

--- FYP/BertModel/src/config.py
+++ FYP/BertModel/src/config.py
@@ -1,9 +1,9 @@
 import transformers
-
+#This file contains global variables for the bert model
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 2
 VALID_BATCH_SIZE = 1
-EPOCHS = 1
+EPOCHS = 10
 BERT_PATH= 'C:/Users/Andrew/Desktop/FinalYearProject/FYP/BertModel/input/bert_based_uncased/'
 MODEL_PATH = 'model.bin'
 TRAINING_FILE = '../FYP/BertModel/input/stock_data.csv'
